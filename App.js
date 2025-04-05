d06deab8a31e0abd94257fc99b6ca6e34e1d6e12
shakicardona <mar.cardona@outlook.es>
2025-04-05 05:24:54 +0200

    google ads integrated

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import mobileAds, { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
 
-export default function App() {
+export default function App({ }) {
+  useEffect(() => {
+    mobileAds().initialize();
+  }, []);
+
+  // Use the test ID for banner ads (or replace with your own production ID)
+  const adUnitId = __DEV__ ? TestIds.BANNER : 'ca-app-pub-3940256099942544/9214589741';
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>React Native Google Mobile Ads Integration</Text>
+      <View style={styles.banner}>
+        <BannerAd
+          unitId={adUnitId}
+          size={BannerAdSize.FULL_BANNER}
+          onAdFailedToLoad={(error) => console.error('Ad failed to load:', error)}
+        />
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flex: 1, 
     justifyContent: 'center',
+    alignItems: 'center'
   },
+
+  banner: {
+    display: 'flex',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    width: '100%',
+    height: '100px'
+  }
 });
+
